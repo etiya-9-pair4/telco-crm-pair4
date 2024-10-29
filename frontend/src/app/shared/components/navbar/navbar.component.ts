8e55f5ba99dd9b515371a95bbc54f38fb27fe583
Eren Macit <ernmctt@gmail.com>
2024-10-29 19:57:59 +0300

    feat: navbar, mainlayout

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Collapse, CollapseInterface } from 'flowbite';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  collapseInstances: { [key: string]: CollapseInterface } = {};
 
+  ngOnInit() {
+    // Dropdownları başlatıyoruz
+    this.initializeCollapse('b2c-toggle', 'b2c-link');
+    this.initializeCollapse('b2b-toggle', 'b2b-link');
+  }
+
+  initializeCollapse(targetId: string, triggerId: string) {
+    const targetEl = document.getElementById(targetId) as HTMLElement;
+    const triggerEl = document.getElementById(triggerId) as HTMLElement;
+
+    if (targetEl && triggerEl) {
+      this.collapseInstances[targetId] = new Collapse(targetEl, triggerEl);
+    }
+  }
+
+  toggleDropdown(targetId: string) {
+    const collapseInstance = this.collapseInstances[targetId];
+    if (collapseInstance) {
+      collapseInstance.toggle();
+    }
+  }
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nav: "#242441",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
