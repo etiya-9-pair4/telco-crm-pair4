7bf05fe00782ade7c586ecb06c38a35be5663641
Eren Macit <ernmctt@gmail.com>
2024-11-04 21:58:17 +0300

    Corrected case sensitivity

--- backend/productservice/src/main/java/com/etiya/productservice/dto/campaignproduct/request/CreateCampaignProductRequestDto.java
+++ backend/productservice/src/main/java/com/etiya/productservice/dto/campaignproduct/request/CreateCampaignProductRequestDto.java
@@ -1,20 +1,19 @@
 package com.etiya.productservice.dto.campaignProduct.request;
 
-import com.etiya.productservice.entity.Campaign;
-import com.etiya.productservice.entity.Product;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateCampaignProductRequestDto {
-    @NotNull(message = "Campaign cannot be null")
-    private Campaign campaign;
-    @NotNull(message = "Campaign cannot be null")
-    private Product product;
+    @NotNull
+    private Long campaignId;
+    @NotNull
+    private Long productId;
 }
