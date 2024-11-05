fc4c0520708d878ea9a5f73089e4338e28530bdb
Eren Macit <ernmctt@gmail.com>
2024-11-05 18:44:31 +0300

    update: createcustomer form => validations and styles

--- frontend/src/app/features/customer/customer.module.ts
+++ frontend/src/app/features/customer/customer.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CreateCustomerComponent } from './pages/create-customer/create-customer.component';
 import { SearchCustomerComponent } from './pages/search-customer/search-customer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CreateCustomerComponent, SearchCustomerComponent],
-  imports: [CommonModule, CustomerRoutingModule],
+  imports: [CommonModule, CustomerRoutingModule, ReactiveFormsModule],
 })
 export class CustomerModule {}
